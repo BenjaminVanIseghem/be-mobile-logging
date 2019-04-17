031d532672cfb344c12a7a35af858a39cf3120a3
Benjamin Van Iseghem <benjamin.vaniseghem.w3891@student.hogent.be>
2019-04-17 16:10:59 +0200

    finished flush method

--- log/bm-logger.go
+++ log/bm-logger.go
@@ -4,46 +4,103 @@ import (
 	"bytes"
 	"io"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
 var (
-	counter  = 0
-	filePath = "/private/etc/promtail/"
-	fileName = "send"
+	fileArr          = []string{}
+	maxNumberOfFiles = 10
+	filePath         = "/private/etc/promtail/"
+	fileName         = "send"
 )
 
-//Flush flushes the buffer to the file which will be scraped to Loki
-func Flush(buf *bytes.Buffer) {
-	file := filePath + fileName + strconv.Itoa(counter) + ".log"
+//LFile is an exported struct with a the buffer to which logs are written and extra info for making a write file
+type LFile struct {
+	buffer        *bytes.Buffer
+	serviceName   string
+	extraPathInfo string
+}
 
-	//Create log file to be scraped to Loki
-	w, err := os.Create(file)
-	if err != nil {
-		panic(err)
-	}
+//Flush flushes the buffer to the file which will be scraped to Loki
+/*
+	If the maximum amount of files is reached, overwrite the oldest file using os.Rename(old, new).
+	This limits the file creation overhead to a certain level.
+	os.Rename(old, new) is optimized for this use case
+*/
+func Flush(logFile LFile) {
 	start := time.Now()
-	n, err := buf.WriteTo(w)
-	if err != nil {
-		panic(err)
+
+	path := filePath + logFile.serviceName + logFile.extraPathInfo + ".log"
+
+	pathInArray := checkPathInArray(path)
+
+	if !pathInArray {
+		if len(fileArr) <= maxNumberOfFiles {
+			//Create log file to be scraped to Loki
+			w, err := os.Create(path)
+			if err != nil {
+				panic(err)
+			}
+			//Write buffer into file
+			n, err := logFile.buffer.WriteTo(w)
+			if err != nil {
+				panic(err)
+			}
+			logrus.Printf("Copied %v bytes\n", n)
+			//Close file
+			w.Close()
+			//Append filepath to array
+			fileArr = append(fileArr, path)
+		} else {
+			//Take oldest filepath and rename this file to new path name
+			err := os.Rename(fileArr[0], path)
+			if err != nil {
+				logrus.Error("Error renaming file", err)
+			}
+			//Open renamed log file, this automatically truncates the existing file
+			w, err := os.Create(fileArr[0])
+			if err != nil {
+				panic(err)
+			}
+			//Write buffer into file
+			n, err := logFile.buffer.WriteTo(w)
+			if err != nil {
+				panic(err)
+			}
+			logrus.Printf("Copied %v bytes\n", n)
+			//Close file
+			w.Close()
+
+			//Use slices to add this file to the back of the array
+			fileArr = append(fileArr[1:], path)
+		}
+	} else {
+		//Open file and flush buffer into this file
+		w, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			panic(err)
+		}
+		//Write buffer into file
+		n, err := logFile.buffer.WriteTo(w)
+		if err != nil {
+			panic(err)
+		}
+		logrus.Printf("Copied %v bytes\n", n)
+		//Close file
+		w.Close()
 	}
-	logrus.Printf("Copied %v bytes\n", n)
 
 	//Reset buffer
-	buf.Reset()
-	//Close file
-	w.Close()
+	logFile.buffer.Reset()
 
-	counter++
-
+	//Calculate flush time
 	logrus.Info("Flushing took: ", time.Since(start))
 }
 
 //CreateLogBuffer creates an in-memory buffer to temporarily store logs
-func CreateLogBuffer() (*bytes.Buffer, *logrus.Entry) {
+func CreateLogBuffer(serviceName string, extraPathInfo string) (LFile, *logrus.Entry) {
 	memLog := &bytes.Buffer{}
 	logger := logrus.New()
 	multiWriter := io.MultiWriter(os.Stdout, memLog)
@@ -52,24 +109,26 @@ func CreateLogBuffer() (*bytes.Buffer, *logrus.Entry) {
 	//Create logrus.Entry
 	entry := logrus.NewEntry(logger)
 
-	return memLog, entry
+	var logFile = LFile{memLog, serviceName, extraPathInfo}
+
+	return logFile, entry
 }
 
 //Error pushes the error onto the buffer and flushes the buffer to file
-func Error(logger *logrus.Entry, msg string, err error, buf *bytes.Buffer) {
+func Error(logger *logrus.Entry, msg string, err error, logFile LFile) {
 	logger.Error(msg, err)
 	//Flush to file
-	Flush(buf)
+	Flush(logFile)
 }
 
 /*
 	Fatal func pushes the error onto the buffer and flushes the buffer to file
 	Afterwards the Fatal function from logrus is called
 */
-func Fatal(logger *logrus.Entry, msg string, err error, buf *bytes.Buffer) {
+func Fatal(logger *logrus.Entry, msg string, err error, logFile LFile) {
 	logger.Error(msg, err)
 	//Flush to file
-	Flush(buf)
+	Flush(logFile)
 	logrus.Fatal(msg)
 }
 
@@ -77,9 +136,51 @@ func Fatal(logger *logrus.Entry, msg string, err error, buf *bytes.Buffer) {
 	Panic func pushes the error onto the buffer and flushes the buffer to file
 	Afterwards the Panic function from logrus is called
 */
-func Panic(logger *logrus.Entry, msg string, err error, buf *bytes.Buffer) {
+func Panic(logger *logrus.Entry, msg string, err error, logFile LFile) {
 	logger.Error(msg, err)
 	//Flush to file
-	Flush(buf)
+	Flush(logFile)
 	logrus.Panic(msg, err)
+}
+
+//ClearBuffers , if a service ends without errors, clear all the buffers before restarting
+func ClearBuffers(buf *[]bytes.Buffer) {
+	//Reset buffers
+	for _, buf := range *buf {
+		buf.Reset()
+	}
+
+}
+
+//Checks if directory is empty before using it
+func isDirEmpty(name string) (bool, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	// read in ONLY one file
+	_, err = f.Readdir(1)
+
+	// and if the file is EOF... well, the dir is empty.
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err
+}
+
+//Check if path is in array
+func checkPathInArray(path string) bool {
+	for _, p := range fileArr {
+		if p == path {
+			return true
+		}
+	}
+	return false
+}
+
+func checkPath() bool {
+
+	return false
 }
